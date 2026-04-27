da6b16b1e2a4f09fd4f1503231e0fca8850a7a8e
Anuj Meena Sharma <anuj.shar2k8@gmail.com>
2026-04-27 13:24:42 +0530

    fixed

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -94,14 +94,18 @@ export default async function LangLayout({ children, params }) {
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
           strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-          `}
-        </Script>
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+            `
+          }}
+        />
         <noscript>
           <img height="1" width="1" style={{ display: "none" }} alt=""
             src="https://www.facebook.com/tr?id=1443902503720485&ev=PageView&noscript=1"
